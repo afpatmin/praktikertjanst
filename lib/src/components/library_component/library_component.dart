9f99b285a71b3436b0a2e9b047796f0498e14564
Patrick <patrick.minogue@gmail.com>
2021-05-19 10:55:11 +0000

    added layman report

--- lib/src/components/library_component/library_component.dart
+++ lib/src/components/library_component/library_component.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
+
 import 'package:angular/angular.dart';
 import 'package:angular/security.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:fo_components/fo_components.dart';
+
 import '../../components/documents_component/documents_component.dart';
 import '../../components/resource_component/resource_component.dart';
 import '../../models/resource.dart';
@@ -28,7 +30,30 @@ import '../../services/video_service.dart';
     templateUrl: 'library_component.html',
     pipes: [CapitalizePipe],
     changeDetection: ChangeDetectionStrategy.Default)
-class LibraryComponent implements OnDestroy {
+class LibraryComponent implements OnInit, OnDestroy {
+  @Input()
+  String backgroundImage;
+
+  final SelectionModel searchModel = SelectionModel<SearchOption>.single();
+
+  StringSelectionOptions<SearchOption> searchOptions;
+
+  bool showModal = false;
+
+  Router router;
+
+  MessagesService msg;
+
+  RiseService riseService;
+  QuizService quizService;
+
+  VideoService videoService;
+  Video selectedModel;
+  final DomSanitizationService sanitizer;
+  List<Resource> resouces = [];
+  StreamSubscription<List<SelectionChangeRecord>> _onSearchSubscription;
+  String laymanReportLabel;
+  String laymanReportLink;
   LibraryComponent(this.router, this.msg, this.riseService, this.quizService,
       this.videoService, this.sanitizer) {
     riseService.data.values.forEach(resouces.add);
@@ -42,15 +67,54 @@ class LibraryComponent implements OnDestroy {
     _onSearchSubscription =
         searchModel.selectionChanges.listen(_onSearchChange);
   }
+  @override
+  void ngOnDestroy() {
+    _onSearchSubscription?.cancel();
+  }
+
+  @override
+  void ngOnInit() {
+    final lang =
+        Uri.base.pathSegments.isEmpty ? 'sv' : Uri.base.pathSegments.first;
+    switch (lang) {
+      case 'de':
+        laymanReportLabel = 'Das Ergebnis des Projekts (EN)';
+        laymanReportLink =
+            'https://www.praktikertjanst.se/globalassets/00.-praktikertjanst-koncernwebb/00.-bilder/life/nyheter/laymansreport-en-webb.pdf';
+        break;
+      case 'en':
+        laymanReportLabel = "The project's results";
+        laymanReportLink =
+            'https://www.praktikertjanst.se/globalassets/00.-praktikertjanst-koncernwebb/00.-bilder/life/nyheter/laymansreport-en-webb.pdf';
+        break;
+      case 'es':
+        laymanReportLabel = 'El resultado del proyecto (EN)';
+        laymanReportLink =
+            'https://www.praktikertjanst.se/globalassets/00.-praktikertjanst-koncernwebb/00.-bilder/life/nyheter/laymansreport-en-webb.pdf';
+        break;
+      case 'fr':
+        laymanReportLabel = 'Le résultat du projet (EN)';
+        laymanReportLink =
+            'https://www.praktikertjanst.se/globalassets/00.-praktikertjanst-koncernwebb/00.-bilder/life/nyheter/laymansreport-en-webb.pdf';
+        break;
+
+      case 'sv':
+        laymanReportLabel = 'Projektrapport';
+        laymanReportLink =
+            'https://www.praktikertjanst.se/globalassets/00.-praktikertjanst-koncernwebb/00.-bilder/life/nyheter/laymansreport-sv-webb.pdf';
+        break;
+
+      default:
+        laymanReportLabel = "The project's results";
+        laymanReportLink =
+            'https://www.praktikertjanst.se/globalassets/00.-praktikertjanst-koncernwebb/00.-bilder/life/nyheter/laymansreport-en-webb.pdf';
+        break;
+    }
+  }
 
   void onVideoClick(Video model) {
     selectedModel = model;
     model.complete = true;
-  }
-
-  @override
-  void ngOnDestroy() {
-    _onSearchSubscription?.cancel();
   }
 
   void _initSearchOptions() {
@@ -90,24 +154,6 @@ class LibraryComponent implements OnDestroy {
       }
     }
   }
-
-  @Input()
-  String backgroundImage;
-
-  final SelectionModel searchModel = SelectionModel<SearchOption>.single();
-  StringSelectionOptions<SearchOption> searchOptions;
-
-  bool showModal = false;
-  Router router;
-  MessagesService msg;
-  RiseService riseService;
-  QuizService quizService;
-  VideoService videoService;
-  Video selectedModel;
-  final DomSanitizationService sanitizer;
-  List<Resource> resouces = [];
-
-  StreamSubscription<List<SelectionChangeRecord>> _onSearchSubscription;
 }
 
 class SearchOption {
